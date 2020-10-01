9bee021b3c10b2a0f2184d1854c2f29a1cd2e702
Kristo Kuusküll <onukristo@gmail.com>
2020-10-01 13:34:50 +0300

    - Upgraded external libraries. - Better error when CuratorFramework is not properly set up.jav

--- tw-leader-selector/src/main/java/com/transferwise/common/leaderselector/SharedReentrantLock.java
+++ tw-leader-selector/src/main/java/com/transferwise/common/leaderselector/SharedReentrantLock.java
@@ -122,9 +122,9 @@ public class SharedReentrantLock implements ILock {
       LostCountingConnectionStateListener.SessionState sessionState = config.lostCountingConnectionStateListener
           .getSessionState();
 
-      if (sessionState == null){
+      if (sessionState == null) {
         throw new IllegalStateException("SessionState is not set. Make sure you use tw-curator and remove your own defined CuratorFramework bean."
-        +" Alternatively get LostCountingConnectionStateListener registered before CuratorFramework is started.");
+            + " Alternatively get LostCountingConnectionStateListener registered before CuratorFramework is started.");
       }
       if (sessionState.isConnected()) {
         return (!config.serverSideCheck || isTheLockIdInTheServerOurs());

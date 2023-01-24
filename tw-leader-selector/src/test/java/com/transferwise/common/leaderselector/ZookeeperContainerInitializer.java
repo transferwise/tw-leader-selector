33687b832e91a05c06d32b7b268dac138f3e58aa
Kristo Kuusküll <kristo.kuuskull@transferwise.com>
2023-01-24 13:57:09 +0200

    Fix.

--- tw-leader-selector/src/test/java/com/transferwise/common/leaderselector/ZookeeperContainerInitializer.java
+++ tw-leader-selector/src/test/java/com/transferwise/common/leaderselector/ZookeeperContainerInitializer.java
@@ -1,14 +1,11 @@
 package com.transferwise.common.leaderselector;
 
-import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.model.Network;
 import java.util.Collections;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.support.TestPropertySourceUtils;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.dockerclient.DockerClientConfigUtils;
 
 @Slf4j
 public class ZookeeperContainerInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
@@ -35,7 +32,7 @@ public class ZookeeperContainerInitializer implements ApplicationContextInitiali
     // Allow Zookeeper to be restarted without needing to reconfigure CuratorFramework port.
     zookeeper.setPortBindings(Collections.singletonList("" + zkPort + ":" + ZOOKEEPER_PORT));
 
-    var connectHost = resolveDockerHost(zookeeper.getDockerClient());
+    var connectHost = zookeeper.getHost();
     var connectString = connectHost + ":" + zkPort;
 
     TestPropertySourceUtils
@@ -46,19 +43,4 @@ public class ZookeeperContainerInitializer implements ApplicationContextInitiali
     return zookeeper;
   }
 
-  private String resolveDockerHost(DockerClient client) {
-    if (DockerClientConfigUtils.IN_A_CONTAINER) {
-      return client.inspectNetworkCmd()
-          .withNetworkId("bridge")
-          .exec()
-          .getIpam()
-          .getConfig()
-          .stream()
-          .filter(it -> it.getGateway() != null)
-          .findAny()
-          .map(Network.Ipam.Config::getGateway)
-          .orElseGet(() -> DockerClientConfigUtils.getDefaultGateway().orElse("localhost"));
-    }
-    return "localhost";
-  }
 }
